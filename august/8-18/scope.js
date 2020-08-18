af084cf2bf70b2176c0bf92611362d42f1ad9d2b
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-18 12:22:37 +0900

    practicing with var function scope and const/let block scope

---
+++ august/8-18/scope.js
@@ -0,0 +1,19 @@
+var avatar = "Ang";
+var element = "Air";
+
+var elements = [
+    "Air",
+    "Earth",
+    "Fire",
+    "Water"
+];
+
+for (var i = 0; i < elements.length; i++) {
+    (function() {
+        var element = elements[i];
+        console.log(avatar + " has mastered " + element);
+    })();
+}
+
+// Outputs: "Ang's primary element is Air"
+console.log(avatar + "'s primary element is " + element);
